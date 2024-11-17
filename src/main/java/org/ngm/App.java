a26566289e7cf71f5bcd75f0450589e697232ed7
Georgii Naumov <gmnaumov@yandex.ru>
2024-11-17 20:32:14 +0300

    feat: add hello-world WebFlux application

--- src/main/java/org/ngm/App.java
+++ src/main/java/org/ngm/App.java
@@ -1,13 +1,17 @@
 package org.ngm;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.ngm.hello.GreetingClient;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(App.class, args);
+
+        GreetingClient client = context.getBean(GreetingClient.class);
+
+        System.out.println(">> message = " + client.getMessage().block());
     }
 }
